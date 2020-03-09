9da596b0e5638e4a14972e10d895b14acb1d1429
Collen A. Aligway <ealmario@proweaver.net>
2020-03-09 18:12:50 +0800

    Last changes for 1st day of 5 day assestment

--- Login.php
+++ Login.php
@@ -1,10 +1,47 @@
 <?php
+    session_start();
     $servername="localhost";
     $username="root";
     $password="";
     $dbname="ray";
 
     $conn= new mysqli($servername,$username,$password,$dbname);
-    $query=mysqli_query($);
 
+    if(isset($_POST['btnSubmit'])){
+
+        $username=$_POST['Username'];
+        $password=$_POST['Password'];
+
+        $result=mysqli_query($conn,"SELECT * FROM user WHERE Username='$username'");
+        $row=mysqli_fetch_assoc($result);
+
+        if($username==$row['Username'] && $password==$row['Password']){
+            if($row['UserType']=="Admin"){
+                if($row['IsActive']){
+                    $_SESSION['UserID']=$row['UserID'];
+                    $_SESSION['Username']=$row['Username'];
+                    $_SESSION['Firstname']=$row['Firstname'];
+                    $_SESSION['UserType']=$row['UserType'];
+                    $_SESSION['IsActive']=$row['IsActive'];
+
+                    echo "<script>window.alert('Hello Admin');window.location='AdminUserView.php';</script>";
+                }else{
+                    echo "<script>window.alert('Hello Admin! Your Account is not Active');window.history.back();</script>";
+                }
+            }else{
+                if($row['IsActive']){
+                    $_SESSION['UserID']=$row['UserID'];
+                    $_SESSION['Username']=$row['Username'];
+                    $_SESSION['Firstname']=$row['Firstname'];
+                    $_SESSION['UserType']=$row['UserType'];
+                    $_SESSION['IsActive']=$row['IsActive'];
+                    echo "<script>window.alert('Hello Customer!');window.history.back();</script>";
+                }else{
+                    echo "<script>window.alert('Hello Customer! Your account is not activated.');window.history.back();</script>";
+                }
+            }
+        }else{
+            echo "<script>window.alert('Invalid Credentials');window.history.back();</script>";
+        }
+    }
  ?>
--- index.php
+++ index.php
@@ -13,14 +13,17 @@
         </header>
         <main>
             <div class="login-box center">
-                <h2 class="text-white text-center">LOGIN ACCOUNT</h1>
-                <div class="row">
-                    <input type="text" class="center" placeholder="Username" name="Username"/>
-                </div>
-                <div class="row">
-                    <input type="password" class="center" placeholder="Password" name="Password"/>
-                </div>
-                <button type="submit" class="btn pink text-white center" name="btnSubmit">SUBMIT</button>
+                <form action="Login.php" method="post">
+                    <h2 class="text-white text-center">LOGIN ACCOUNT</h1>
+                    <div class="row">
+                        <input type="text" class="center" placeholder="Username" name="Username"/>
+                    </div>
+                    <div class="row">
+                        <input type="password" class="center" placeholder="Password" name="Password"/>
+                    </div>
+                    <button type="submit" class="btn pink text-white center" name="btnSubmit">SUBMIT</button>
+                </form>
+
             </div>
         </main>
         <footer>
